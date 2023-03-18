c4ed53fef317ec50f5cfc464d78479531529c391
taldr27 <diegogarciaq98@gmail.com>
2023-03-18 18:14:27 -0500

    Add ai config for code bot chat

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -3,6 +3,7 @@ import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-
 import Header from "@/components/customHeader";
 import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm"
 import Ai from '@/components/customMessageForms/Ai';
+import AiCode from '@/components/customMessageForms/AiCode';
 
 const Chat = () => {
   const chatProps = useMultiChatLogic(
@@ -21,6 +22,9 @@ const Chat = () => {
         renderMessageForm={(props) => {
           if (chatProps.chat?.title.startsWith("AiChat_")) {
             return <Ai props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiCode_")) {
+            return <AiCode props={props} activeChat={chatProps.chat} />;
           }
           
           return (
--- client/src/state/api.js
+++ client/src/state/api.js
@@ -12,7 +12,14 @@ export const api = createApi({
         body: payload,
       }),
     }),
+    postAiCode: build.mutation({
+      query: (payload) => ({
+        url: "openai/code",
+        method: "POST",
+        body: payload,
+      }),
+    }),
   }),
 });
 
-export const { usePostAiTextMutation } = api;
+export const { usePostAiTextMutation, usePostAiCodeMutation } = api;

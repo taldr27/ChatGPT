4421e2dadb0e284e250e18f89d5248460e11e59f
taldr27 <diegogarciaq98@gmail.com>
2023-03-19 13:52:57 -0500

    Add condition to create an autocomplete chat

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -4,6 +4,7 @@ import Header from "@/components/customHeader";
 import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm"
 import Ai from '@/components/customMessageForms/Ai';
 import AiCode from '@/components/customMessageForms/AiCode';
+import AiAssist from '@/components/customMessageForms/AiAssist';
 
 const Chat = () => {
   const chatProps = useMultiChatLogic(
@@ -25,6 +26,9 @@ const Chat = () => {
           }
           if (chatProps.chat?.title.startsWith("AiCode_")) {
             return <AiCode props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiAssist_")) {
+            return <AiAssist props={props} activeChat={chatProps.chat} />;
           }
           
           return (
